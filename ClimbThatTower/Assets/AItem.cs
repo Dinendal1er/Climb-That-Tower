b49f6bb94201ad7c958e90acb5d54e12697322b6
Drowmar <vincent.morin@epitech.eu>
2016-02-15 18:04:12 +0100

    AItem get and set

--- ClimbThatTower/Assets/AItem.cs
+++ ClimbThatTower/Assets/AItem.cs
@@ -8,15 +8,52 @@ public class AItem {
     private string _describ;
     private Texture2D _sprite;
     private int _talent;
-    private enum _type;
 
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            _name = value;
+        }
+    }
+
+    public string describ
+    {
+        get
+        {
+            return _describ;
+        }
+        set
+        {
+            _describ = value;
+        }
+    }
+
+    public Texture2D sprite
+    {
+        get
+        {
+            return _sprite;
+        }
+        set
+        {
+            _sprite = value;
+        }
+    }
+
+    public int talent
+    {
+        get
+        {
+            return _talent;
+        }
+        set
+        {
+            _talent = value;
+        }
+    }
 }
