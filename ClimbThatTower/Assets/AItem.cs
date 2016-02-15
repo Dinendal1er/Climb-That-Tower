55c39b2e40cc9ea4537fdcbe0d25e1dcdb5cc37a
Drowmar <vincent.morin@epitech.eu>
2016-02-15 17:42:47 +0100

    AItem

---
+++ ClimbThatTower/Assets/AItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum eItemType { WEAPON, HEAD, TORSO, PANTS, FOOT, HAND, CONSUMABLE, ONEHANDED, DOUBLEHANDED, RIGHTHAND, LEFTHAND}
+
+public class AItem {
+    private string _name;
+    private string _describ;
+    private Texture2D _sprite;
+    private int _talent;
+    private enum _type;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
