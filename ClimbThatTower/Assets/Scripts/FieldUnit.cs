529d45ad38f37fa19810b5eb540cc5a45600c6d5
coudra_c <colin.coudray@epitech.eu>
2016-02-16 18:32:39 +0100

    Push d'une map en dur avec 3 murs et 10 sols disponibles

---
+++ ClimbThatTower/Assets/Scripts/FieldUnit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldUnit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter()
+	{
+		GetComponent<Renderer> ().material = Resources.Load ("Materials/" + (this.name.Split ('(')) [0] + "hl") as Material;
+	}
+
+	void OnMouseExit()
+	{
+		GetComponent<Renderer> ().material = Resources.Load ("Materials/" + (this.name.Split ('(')) [0]) as Material;
+	}
+}
---
+++ ClimbThatTower/Assets/Scripts/Viewer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Viewer : MonoBehaviour {
+
+	public float speed = 7F;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+		
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.RightArrow) )//|| Input.mousePosition.x == Screen.width)
+		{
+			transform.Translate(Vector3.right * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow) )//|| Input.mousePosition.x == 0)
+		{
+			transform.Translate(Vector3.left * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.DownArrow) )//|| Input.mousePosition.y == 0)
+		{
+			transform.Translate(Vector3.down * Time.deltaTime * speed);
+		}
+		if (Input.GetKey (KeyCode.UpArrow) )//|| Input.mousePosition.y == Screen.height)
+		{
+			transform.Translate(Vector3.up * Time.deltaTime * speed);
+		}
+	}
+}
