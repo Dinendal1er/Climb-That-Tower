ab806386ae3560d6a4a494fcdd250e10e3d2013f
lopez_t <thibaut.lopez@epitech.eu>
2016-02-23 10:40:38 +0100

    Making basic animation

---
+++ ClimbThatTower/Assets/CharacterTurner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CharacterTurner : MonoBehaviour
+{
+    public string characterName;
+    private GameObject _player;
+
+    void Start()
+    {
+        this._player = GameObject.Find(this.characterName);
+    }
+
+    void Update()
+    {
+        HasClicked click;
+        Animator animator;
+
+        animator = this._player.GetComponent<Animator>();
+        foreach (Transform child in this.gameObject.transform)
+        {
+            click = child.GetComponent<HasClicked>();
+            if (click.hasClicked())
+            {
+                print(child.name);
+                animator.SetBool("Up", (child.name == "Up") ? true : false);
+                animator.SetBool("Down", (child.name == "Down") ? true : false);
+                animator.SetBool("Left", (child.name == "Left") ? true : false);
+                animator.SetBool("Right", (child.name == "Right") ? true : false);
+                animator.SetBool("Up-Right", (child.name == "Up-Right") ? true : false);
+                animator.SetBool("Up-Left", (child.name == "Up-Left") ? true : false);
+                animator.SetBool("Down-Right", (child.name == "Down-Right") ? true : false);
+                animator.SetBool("Down-Left", (child.name == "Down-Left") ? true : false);
+            }
+        }
+    }
+}
